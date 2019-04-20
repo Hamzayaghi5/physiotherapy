9b361c097a9cd00dc7768b6cd4bdd3bcd1eedfda
Hamzayaghi5 <hamzayaghi2@gmail.com>
2019-04-20 23:47:30 +0300

    done adding admin views and service controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Auth;
+use App\User;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -25,7 +28,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/admin_page';
 
     /**
      * Create a new controller instance.
@@ -36,4 +39,30 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+       public function login(Request $request)
+  {
+    $email=$request['email'];
+    $password=$request['password'];
+      //$credentials = $request->only('email', 'password');
+
+      if (Auth::attempt(['email' => $email, 'password' => $password])) {
+          // Authentication passed...
+        $user= User::where('email',$request->email)->first();
+        if($user->is_admin())
+        {
+          return redirect('/admin_page');
+        }
+        else {
+          return redirect('/home');
+        }
+
+      }
+      return redirect()->intended('/login');
+  }
+
+  public function logout(Request $request) {
+  Auth::logout();
+  return redirect('/');
+}
 }
--- app/User.php
+++ app/User.php
@@ -16,8 +16,36 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password','type',
     ];
+
+    public static function get_all()
+    {
+        $users=User::all();
+        return $users;
+    }
+
+     public static function get($id)
+    {
+        $user=User::find($id);
+        return $user;
+    }
+
+
+
+
+       public function is_admin()
+     {
+       if($this->type=='admin')
+       {
+         return true;
+       }
+       else {
+         // code...
+         return false;
+       }
+     }
+
 
     /**
      * The attributes that should be hidden for arrays.
@@ -26,14 +54,5 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
