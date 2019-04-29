4082f4e82812b19923a5e1ab2ce1a2758a99d23c
Hamzayaghi5 <hamzayaghi2@gmail.com>
2019-04-30 02:36:01 +0300

    done making responsive

--- resources/views/main_site/service_single.blade.php
+++ resources/views/main_site/service_single.blade.php
@@ -15,20 +15,20 @@
                 <h3 class="title-w3pvt mb-5">{{$service->title}}</h3>
             </div>
 
-            <div class="single-w3pvt-page mt-md-5 mt-4 px-lg-5">
-                <div class="content-sing-w3layouts px-lg-5">
+         
                 	
                 	@foreach ($service->images as $image)
                 	<div class="row">
                 	<img class="img-fluid img-responsive mx-auto d-block" src="{{env('image_storage')}}/{{$image->image}}" alt="">
                 	</div>
                 	@endforeach
-                    
+                    <div class="row col-lg">
+                        
+                 
                     <h4>what is {{$service->title}}:</h4>
                     <p>{!!$service->main_description!!}</p>
-
-                </div>
-            </div>
+   </div>
+          
         </div>
 
 
--- resources/views/main_site/why_choose_us.blade.php
+++ resources/views/main_site/why_choose_us.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container" style="margin-bottom:5%;margin-top:15%">
 		<div class="row">
-		<div class="col-6">
+		<div class="col-lg">
 			<h1 style="font-weight: bold;color:#777;margin-bottom:5%;">{{$page->name}}:</h1>
 		</div>
 	</div>
 	<div class="row">
 
-	<div class="col-6">
+	<div class="col-lg">
 		<p style="font-size: 15pt;">{!!$page->description!!}</p>
 	</div>
 
-		<div class="col-6">
+		<div class="col-lg">
 			<img src="{{env('image_storage')}}/{{$page->image}}" class="img-fluid">
 	</div>
 	</div>
